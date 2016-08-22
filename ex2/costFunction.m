3b0e9b95700492a685137dde233058aae45c4645
Miguel Pousa <miguelpousafreire@gmail.com>
2016-08-23 00:30:36 +0200

    first session: completed 70/100

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -21,8 +21,11 @@ grad = zeros(size(theta));
 %
 
 
+h = sigmoid(X*theta);
+sub_m = 1 / m;
+J = sub_m * sum(-y'*log(h) - (1-y)' * log(1-h));
 
-
+grad = sub_m * ((h-y)'*X);
 
 
 
--- ex2/ex2.m
+++ ex2/ex2.m
@@ -33,25 +33,25 @@ X = data(:, [1, 2]); y = data(:, 3);
 fprintf(['Plotting data with + indicating (y = 1) examples and o ' ...
          'indicating (y = 0) examples.\n']);
 
-plotData(X, y);
+%plotData(X, y);
 
 % Put some labels
-hold on;
+%hold on;
 % Labels and Legend
-xlabel('Exam 1 score')
-ylabel('Exam 2 score')
+%xlabel('Exam 1 score')
+%ylabel('Exam 2 score')
 
 % Specified in plot order
-legend('Admitted', 'Not admitted')
-hold off;
+%legend('Admitted', 'Not admitted')
+%hold off;
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+%fprintf('\nProgram paused. Press enter to continue.\n');
+%pause;
 
 
 %% ============ Part 2: Compute Cost and Gradient ============
 %  In this part of the exercise, you will implement the cost and gradient
-%  for logistic regression. You need to complete the code in 
+%  for logistic regression. You need to complete the code in
 %  costFunction.m
 
 %  Setup the data matrix appropriately, and add ones for the intercept term
@@ -92,7 +92,7 @@ fprintf('theta: \n');
 fprintf(' %f \n', theta);
 
 % Plot Boundary
-plotDecisionBoundary(theta, X, y);
+%plotDecisionBoundary(theta, X, y);
 
 % Put some labels
 hold on;
